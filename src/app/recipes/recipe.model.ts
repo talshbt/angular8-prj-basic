c7ddcdce8f68126eeed4ac670b8c9e8393a267ec
Tal Shabtai <talshbt@gmail.com>
2019-10-13 11:35:58 +0300

    120. Adding Ingredients to Recipes

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,20 @@
+import { Ingerdient } from "../shared/ingredient.model";
+
 export class Recipe{
     public name: string;
     public description:string;
     public imagePath:string;
-    constructor(name:string, desc:string, imagePath:string){
+    public ingredients:Ingerdient[];
+
+    
+
+    constructor(name:string, desc:string, imagePath:string, ingredients: Ingerdient[]){
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
+        this.ingredients = ingredients;
+        
     }
+
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,30 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingerdient } from '../shared/ingredient.model';
 
 
 export class RecipeService {
 
    recipeSelected = new EventEmitter<Recipe>();
+
    private recipes:Recipe[] = [
-        new Recipe('Test Recipe','This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg') ,
-        new Recipe('Another Test Recipe','This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe('Test Recipe',
+                  'This is simply a test', 
+                  'https://get.pxhere.com/photo/burger-vegetarian-food-dish-hamburger-cuisine-ingredient-breakfast-sandwich-veggie-burger-slider-fast-food-junk-food-produce-sandwich-finger-food-salmon-burger-buffalo-burger-Burger-king-premium-burgers-bun-recipe-cheeseburger-blt-meat-american-food-comfort-food-baked-goods-appetizer-fried-food-Original-chicken-sandwich-patty-1586555.jpg',
+                  [
+                    new Ingerdient('Meat', 1),
+                    new Ingerdient('French Fried', 3)
+
+                  ]) ,
+        new Recipe('Another Test Recipe',
+                  'This is simply a test', 
+                  'https://images.pexels.com/photos/166655/pexels-photo-166655.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+                  [
+                    new Ingerdient('Buns', 2),
+                    new Ingerdient('Meat', 4)
+
+                  ])
       ];
-
-
 
 
     getRecipe(){
