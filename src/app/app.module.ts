d855d0f8aa36ce4341ba97382a1266e5267d97c0
Tal Shabtai <talshbt@gmail.com>
2019-10-16 16:03:42 +0300

    159. Adding Child Routing Together

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRouteModule } from './app-rout.module';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,7 @@ import { AppRouteModule } from './app-rout.module';
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    
+    RecipeStartComponent,
   ],
   imports: [
     BrowserModule,
